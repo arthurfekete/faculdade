1781f7e484d462c002f1344dff1f264bc471e232
Arthur Fekete <94174231+arthurfekete@users.noreply.github.com>
2024-03-22 14:52:05 -0300

    Add files via upload

--- Faculdade/exercicio3.java
+++ Faculdade/exercicio3.java
@@ -7,38 +7,39 @@ public class exercicio3 {
         int idade;
         double salario;
 
-      do{
-        System.out.print("Digite o nome: ");
-        nome = teclado.nextLine();
-      } while (nome.length() <= 3);
+        do {
+            System.out.print("Digite o nome: ");
+            nome = teclado.nextLine();
+        } while (nome.length() <= 3);
 
-      do{
-        System.out.print("Digite a idade: ");
-        idade = teclado.nextInt();
-      } while (idade < 0 || idade > 150);
+        do {
+            System.out.print("Digite a idade: ");
+            idade = teclado.nextInt();
+        } while (idade < 0 || idade > 150);
 
-      do{
-        System.out.print("Informe o sexo 'f' ou 'm': ");
-        sexo = teclado.next().toLowerCase();
-      } while (!sexo.equals("f") && !sexo.equals("m"));
+        do {
+            System.out.print("Informe o sexo 'f' ou 'm': ");
+            sexo = teclado.next().toLowerCase();
+        } while (!sexo.equals("f") && !sexo.equals("m"));
 
-      do{
-        System.out.print("Informe o estado civil 's', 'c', 'v' ou 'd': ");
-        estadoCivil = teclado.next().toLowerCase();
-      } while (!estadoCivil.equals("s") && !estadoCivil.equals("c") && !estadoCivil.equals("v") && !estadoCivil.equals("d"));
+        do {
+            System.out.print("Informe o estado civil 's', 'c', 'v' ou 'd': ");
+            estadoCivil = teclado.next().toLowerCase();
+        } while (!estadoCivil.equals("s") && !estadoCivil.equals("c") && !estadoCivil.equals("v")
+                && !estadoCivil.equals("d"));
 
-      do{
-        System.out.print("Informe o salário: ");
-        salario = teclado.nextDouble();
-      } while (salario < 0);
+        do {
+            System.out.print("Informe o salário: ");
+            salario = teclado.nextDouble();
+        } while (salario < 0);
 
-      System.out.println("\nInformações válidas:");
-      System.out.println("Nome: " + nome);
-      System.out.println("Idade: " + idade);
-      System.out.println("Salário: " + salario);
-      System.out.println("Sexo: " + (sexo.equals("f") ? "Feminino" : "Masculino"));
-      System.out.println("Estado Civil: " + estadoCivil.toUpperCase());
+        System.out.println("\nInformações válidas:");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salario);
+        System.out.println("Sexo: " + (sexo.equals("f") ? "Feminino" : "Masculino"));
+        System.out.println("Estado Civil: " + estadoCivil.toUpperCase());
 
-      teclado.close();
+        teclado.close();
     }
 }
